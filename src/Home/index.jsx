296728831cd84849ef2e6bd18b06caff22b93ba8
Ghamb <gustavo.hamburg@outlook.com>
2022-06-06 23:29:38 -0300

    feat: start project with all pages

---
+++ src/Home/index.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useState } from 'react'
+import{HeartIcon} from'@heroicons/react/outline'
+
+const MAX_TWT_CHAR =140
+function TeewtForm(){
+   const [text, setText] = useState("")
+
+   function changeText(e){
+     setText(e.target.value)
+   }
+   console.log(text)
+  
+  return(
+    <div className='border-bottom border-silver p-4 space-y-6'>
+      <div className='flex p-4 space-x-5'>
+        <img src="src/avatar.png" alt="" className='w-7' />
+        <h1 className='font-bold text-xl '>pagina Inicial</h1>
+
+      </div>
+      <form className='pl-12 text-lg flex flex-col'>
+        <textarea name="text" value={text} className='bg-transparent outline-none disabled:opacity-40'  placeholder='O que esta acontecendo ?' onChange={changeText} />
+
+        <div className='flex justify-end items-center space-x-3'>
+          <span className='text-sm'>
+            <span >{text.length}</span> / <span className='text-birdBlue'>{MAX_TWT_CHAR}</span>
+          </span>
+          <button className='bg-birdBlue px-5 py-2 rounded-full disabled:opacity-40' disabled={text.length > MAX_TWT_CHAR } >
+            Tweet
+          </button>
+        </div>
+        
+      </form>
+    </div>
+  )
+}
+
+function Tweet({name,username,avatar,children}){
+  return(
+    <div className='flex space-x-3 p-4 border-b border-silver'>
+      <div className="">
+            <img src={avatar} className="tweet_img"/>
+      </div>
+      <div className='space-y-1'>
+            <span className='font-bold text-sm'>{name}</span>{' '}
+            <span className='text-sm text-silver'>@{username}</span>
+            <p>
+                 {children}
+            </p>
+            <div className='flex space-x-1 text-silver text-sm items-center'>
+              <HeartIcon className='w-5 stroke-1 '/>
+              <span>18.9k</span>
+            </div>
+            
+            
+      </div>
+    </div>
+  )
+}
+
+export function Home() {
+  return (
+    <>
+    <TeewtForm/>
+      <Tweet name="Elon Musk" username="elonMusk" avatar="src/avatar.png">
+        Let's make Twitter maximun fun!
+      </Tweet>
+      <Tweet name="Gustavo Hamburg" username="Ghamb" avatar="src/avatar.png">
+        Hambs the Ghambitus
+      </Tweet>
+      
+    </>
+  )
+}
+
---
+++ src/Login/index.jsx
@@ -0,0 +1,69 @@
+
+import {useFormik} from 'formik'
+import * as yup from 'yup'
+import axios from 'axios'
+
+const Input = props =>(
+    
+    <input {...props} className="w-full bg-transparent p-4 border rounded-xl border-onix text-lg outline-none" />
+
+)
+
+const validationSchema = yup.object({
+    email: yup.string().required('Digite seu E-mail').email('E-mail Inválido'),
+    password: yup.string().required('Digite sua Senha')
+})
+export function Login({signInUser}){
+       
+        const formik = useFormik({
+            onSubmit: async values=>{ 
+               const res = await axios.get('http://localhost:9901/login',{
+                    auth:{
+                        username:values.email,
+                        password:values.password
+                    }
+                })
+
+                signInUser(res.data)
+            },
+                initialValues:{
+                    email:'',
+                    password:''
+                },
+                validationSchema,
+                validateOnMount:true,
+            
+    })
+    return(
+        <div className='h-full flex  justify-center'>
+            <div className='bg-birdBlue lg:flex-1 '>
+            </div>
+            <div className="flex-1 flex justify-center items-center p-12 space-y-6  ">
+                <div className='max-w-md flex-1'>
+                    <h1 className="text-3xl">Acesse sua conta</h1>
+
+                    <form className="space-y-6" onSubmit={formik.handleSubmit} >
+                        <div className='space-y-2'>
+                            <Input placeholder="Email" type="text" name="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} disabled={formik.isSubmitting}/>
+                            {(formik.touched.email && formik.errors.email ) && (<div className='text-red-500 text-sm  '> {formik.errors.email}</div>)}
+                        </div>
+                        <div className='space-y-2'>
+                            <Input placeholder="Password" type="password" name="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} disabled={formik.isSubmitting}/>
+                            {(formik.touched.password && formik.errors.password ) && (<div className='text-red-500 text-sm  '> {formik.errors.password}</div>)}
+                        </div>
+                    
+                    
+
+                    
+
+                        <button type='submit' disabled={formik.isSubmitting || !formik.isValid}className="w-full bg-birdBlue py-4 rounded-full disabled:opacity-40 text-lg">{formik.isSubmitting?'Enviando':'Entrar'}</button>
+                    </form>
+
+                    <span className="text-sm text-silver text-center">
+                        Não tem conta? <a href='/signup' className="text-birdBlue">Inscreva-se</a>
+                    </span>
+                </div>
+            </div>
+        </div>    
+    )
+}
