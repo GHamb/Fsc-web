db7071bb02b1857426e7fa9c03a65026f3052247
Ghamb <gustavo.hamburg@outlook.com>
2022-06-07 02:20:48 -0300

    fix changes avatar

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import{HeartIcon} from'@heroicons/react/outline'
 import axios from 'axios'
+import avatar from './avatar.png'
 
 import {useFormik} from 'formik'
 
@@ -40,7 +41,7 @@ function TweetForm({loggedInUser, OnSuccess}){
   return(
     <div className='border-bottom border-silver p-4 space-y-6'>
       <div className='flex p-4 space-x-5'>
-        <img src="./avatar.png" alt="" className='w-7' />
+        <img src={avatar} alt="" className='w-7' />
         <h1 className='font-bold text-xl '>pagina Inicial</h1>
 
       </div>
